8047a2cc561ca1fd95aeb71e405f29e3e79b0613
Mateusz Konieczny <matkoniecz@gmail.com>
2026-04-10 13:17:28 +0200

    try start rendering from z7 again

--- app/src/frontend/map/layers/building-data-layer.tsx
+++ app/src/frontend/map/layers/building-data-layer.tsx
@@ -100,7 +100,7 @@ export function BuildingDataLayer({
                 updateInterval={200}
                 key={`${tileset}-${revisionId}`}
                 url={getTileLayerUrl(tileset, { rev: revisionId })}
-                minZoom={9}
+                minZoom={7}
                 maxZoom={19}
                 detectRetina={false}
                 eventHandlers={{
